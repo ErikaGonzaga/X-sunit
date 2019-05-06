29c5d8ac0190865a2910a3b3a4997c32219a5b70
Erika Gonzaga <erikagonzaga@alu.ufc.br>
2019-05-05 23:53:53 -0300

    initial version

---
+++ db/migrate/20190506013855_create_survivors.rb
@@ -0,0 +1,12 @@
+class CreateSurvivors < ActiveRecord::Migration[5.2]
+  def change
+    create_table :survivors do |t|
+      t.string :name
+      t.integer :age
+      t.string :sex
+      t.string :captured
+
+      t.timestamps
+    end
+  end
+end
